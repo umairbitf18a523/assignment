d3be4baefd2c5d4dbe9394f4ed04e52d0bdba7cb
usama <bitf18a534@pucit.edu.pk>
2021-10-24 21:18:08 +0500

    create references of components

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText Text1= (EditText) findViewById(R.id.editTextTextPersonName);
+        EditText Text2= (EditText) findViewById(R.id.editTextTextPersonName2);
+        EditText Text3= (EditText) findViewById(R.id.editTextTextPersonName3);
+        EditText Text4= (EditText) findViewById(R.id.editTextTextPersonName4);
+        TextView TextView= (TextView) findViewById((R.id.textView));
+        Button result=(Button) findViewById(R.id.button);
+
+
     }
 }
